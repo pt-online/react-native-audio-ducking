2058cfd6f34b64c7c8ae5aeab74d38e27a9f03eb
Markus Hilding <markushilding@gmail.com>
2022-12-20 14:30:15 +0100

    Specify requireMainQueueSetup
    Fixes warning: "Module AudioDucking requires main queue setup since it overrides `init` but doesn't implement `requiresMainQueueSetup`. In a future release React Native will default to initializing all native modules on a background thread unless explicitly opted-out of."
    
    requireMainQueueSetup returns NO as the module does not need to access to UIKit.
    
    More info here: https://reactnative.dev/docs/native-modules-ios#exporting-constants

--- ios/AudioDucking.m
+++ ios/AudioDucking.m
@@ -2,6 +2,11 @@
 
 @interface RCT_EXTERN_MODULE(AudioDucking, NSObject)
 
++ (BOOL)requiresMainQueueSetup
+{
+    return NO;
+}
+
 RCT_EXTERN_METHOD(duckAudio:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject)
 RCT_EXTERN_METHOD(removeAudioDucking:(RCTPromiseResolveBlock)resolve
